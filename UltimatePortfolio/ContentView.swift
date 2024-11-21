1fd6c0975cc410dbbb8aafde48b38c61eccfbb0e
Brody Roberts <brodyroberts2002@gmail.com>
2024-11-21 11:40:55 -0800

    Minute 13:00 in video Showing, deleting, and synchronizing issues

--- UltimatePortfolio/ContentView.swift
+++ UltimatePortfolio/ContentView.swift
@@ -8,8 +8,33 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @EnvironmentObject var dataController: DataController
+
+    var issues: [Issue] {
+        let filter = dataController.selectedFilter ?? .all
+        var allIssues: [Issue]
+        
+        if let tag = filter.tag{
+            allIssues = tag.issues?.allObjects as? [Issue] ?? []
+        } else{
+            let request = Issue.fetchRequest()
+            
+            // Core data doesn't under stand Swift's newer dataType so this line is needed...
+            request.predicate = NSPredicate(format: "modificationDate > %@", filter.minModificationDate as NSDate)
+            allIssues = (try? dataController.container.viewContext.fetch(request)) ?? []
+        }
+        
+        return allIssues.sorted()
+    }
+    
     var body: some View {
-        Text("Content")
+        List {
+            ForEach(issues){ issue in
+                IssueRow(issue: issue)
+            }
+        }
+        .navigationTitle("Issues")
     }
 }
 
